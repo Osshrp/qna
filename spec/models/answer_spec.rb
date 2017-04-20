93642f38b362b1d09742ff03190fb7a286043e85
Osshrp <osshrp@gmail.com>
2017-04-20 10:33:49 +0300

    validatr question_id

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,14 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  let(:question) { Question.create(title: '123', body: '123') }
-  let(:answer) { Answer.create(body: '123', question: question) }
-
   it { should validate_presence_of :body }
+  it { should validate_presence_of :question_id }
   it { should belong_to(:question) }
   it { should have_db_index(:question_id) }
-
-  it 'check_presence_of_question_id' do
-    answer.question_id.should equal(question.id)
-  end
 end
