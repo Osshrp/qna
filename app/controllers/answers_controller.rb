2c5e034ec9e45052ec43177369ee33308073595b
Osshrp <osshrp@gmail.com>
2017-04-21 16:18:43 +0300

    delete some actions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,20 +1,8 @@
 class AnswersController < ApplicationController
   before_action :set_question, only: [:index, :new, :create]
-  before_action :set_answer, only: [:edit]
-
-  def index
-    @answers = @question.answers
-  end
-
-  def show
-    @answer = Answer.find(params[:id])
-  end
 
   def new
     @answer = @question.answers.new
-  end
-
-  def edit
   end
 
   def create
@@ -35,9 +23,5 @@ class AnswersController < ApplicationController
 
   def set_question
     @question = Question.find(params[:question_id])
-  end
-
-  def set_answer
-    @answer = Answer.find(params[:id])
   end
 end
