7538f7e3acb06211c855baae2dda213409ec7cdc
Osshrp <osshrp@gmail.com>
2017-04-25 14:09:00 +0300

    view question spec

--- spec/acceptance/view_question_spec.rb
+++ spec/acceptance/view_question_spec.rb
@@ -5,7 +5,14 @@ feature 'View question', %q{
   I need to be able to viiew question
   and answers to that question
 } do
+  given(:user) { create(:user) }
+  given(:answer) { create(:answer) }
 
-  scenario 'Authenticated user views the question and answers'
-  scenario 'Unauthenticated user views the questions and answers'
+  scenario 'Authenticated user views the question and answers to that question' do
+    sign_in(user)
+    visit_and_check_question
+  end
+  scenario 'Unauthenticated user views the questions and answers to that question' do
+    visit_and_check_question
+  end
 end
