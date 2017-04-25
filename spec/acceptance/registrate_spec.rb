25c6213af7d7a120c22bf9b4acd5ed5710ce08c2
Osshrp <osshrp@gmail.com>
2017-04-25 14:41:38 +0300

    registrate spec

--- spec/acceptance/registrate_spec.rb
+++ spec/acceptance/registrate_spec.rb
@@ -4,6 +4,29 @@ feature 'User registrates', %q{
   In oder to sign in
   I need to be able to registrate in system
 } do
-  scenario 'Unregistered user try to registrate'
-  scenario 'Registered user try to registrate'
+
+  given(:user) { create(:user) }
+
+  scenario 'Unregistered user tries to registrate' do
+    visit root_path
+    click_on 'Ask question'
+    click_on 'Sign up'
+    fill_in 'Email', with: 'vvv@test.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully'
+    expect(page).to have_content 'Sign out'
+    expect(page).to have_content 'vvv@test.com'
+  end
+
+  scenario 'Authenticated user tries to registrate' do
+    sign_in(user)
+
+    visit root_path
+    click_on 'Ask question'
+
+    expect(page).to have_no_content 'Sign up'
+  end
 end
