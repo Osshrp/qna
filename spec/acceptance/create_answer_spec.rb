77a877990c3291883b03f61ff7f5a54f15e363a1
Osshrp <osshrp@gmail.com>
2017-04-29 14:13:56 +0300

    add li with jquery

--- spec/acceptance/create_answer_spec.rb
+++ spec/acceptance/create_answer_spec.rb
@@ -19,18 +19,8 @@ feature 'Create answer', %q{
     expect(page).to have_content 'Answer text'
   end
 
-  scenario 'Authenticated user tries to create answer with blank body field' do
-    sign_in(user)
-    visit question_path(question)
-    click_on 'Create answer'
-
-    expect(page).to have_content "Body can't be blank"
-  end
   scenario 'Unauthenticated user answers the question' do
     visit question_path(question)
-
-    # fill_in 'answer_body', with: 'Answer text'
-    # click_on 'Create answer'
 
     expect(page).to have_no_selector('input')
   end
