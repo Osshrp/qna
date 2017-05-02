7c126157c71f50cf29369023dfa0cd9171e42d0d
Osshrp <osshrp@gmail.com>
2017-05-02 15:04:31 +0300

    fix errors

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -31,14 +31,14 @@ RSpec.describe AnswersController, type: :controller do
     context 'with invalid attributes' do
       it 'does not save the answer' do
         expect { post :create, params: { question_id: question,
-                 answer: attributes_for(:invalid_answer) } }
+                 answer: attributes_for(:invalid_answer) }, format: :js }
           .to_not change(Answer, :count)
       end
 
       it 'renders to question show view' do
         post :create, params: { question_id: question,
-                                answer: attributes_for(:invalid_answer) }
-        expect(response).to render_template('questions/show')
+                                answer: attributes_for(:invalid_answer) }, format: :js
+        expect(response).to render_template('answers/create')
       end
     end
   end
