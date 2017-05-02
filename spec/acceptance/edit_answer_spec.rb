b2883a0961088c4eb3e92f5c5e0870ef36b84618
Osshrp <osshrp@gmail.com>
2017-05-02 06:49:24 +0300

    to work

---
+++ spec/acceptance/edit_answer_spec.rb
@@ -0,0 +1,28 @@
+require_relative 'acceptance_helper'
+
+feature 'Answer editing', %q{
+  In order to fix mistake
+  as an author of answer
+  I'd like to be able to edit answer
+} do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given(:answer) { create(:answer, question: question, user_id: user.id) }
+
+  scenario 'Unuthenticated user tries to edit answer' do
+    visit question_path(question)
+
+    expect(page).to_not have_link 'Edit'
+  end
+  scenario 'Author sees answer edit ling' do
+    sign_in(user)
+    visit question_path(question)
+
+    within '.answers' do
+      expect(page).to have_link 'Edit'
+    end
+  end
+  scenario 'Author tries to edit his answer'
+  scenario "Authenticated user tries to edit other user's answer"
+end
