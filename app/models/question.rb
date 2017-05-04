5eeb58b48bace7743c20853fa620c02162f66ee6
Osshrp <osshrp@gmail.com>
2017-05-04 15:42:05 +0300

    Question#the_best_answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,19 @@ class Question < ApplicationRecord
   belongs_to :user
 
   validates :title, :body, presence: true
+
+  def best_answer(answer)
+    clear_best_answer
+    answer.best = true
+    answer.save
+  end
+
+  private
+
+  def clear_best_answer
+    answers.where(best: true).each do |answer|
+      answer.best = false
+      answer.save
+    end
+  end
 end
