ac5cc1fe21ccbe52c91a64aef24608f105c59c68
Osshrp <osshrp@gmail.com>
2017-05-04 15:42:05 +0300

    Question#the_best_answer

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Question, type: :model do
   it { should belong_to(:user) }
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+
+  let(:question) { create(:question_with_answers) }
+  describe '#best_answer' do
+    it 'should set best answer' do
+      answer = question.answers.first
+      question.best_answer(answer)
+      answer.reload
+      expect(answer.best).to be_truthy
+    end
+  end
 end
