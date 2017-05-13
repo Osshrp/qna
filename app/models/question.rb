33e63a7b616d4a91d3cf39199cad616fc37e313b
Osshrp <osshrp@gmail.com>
2017-05-13 14:09:34 +0300

    concern attachable

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,6 @@
 class Question < ApplicationRecord
+  include Attachable
   has_many :answers, dependent: :destroy
-  has_many :attachments, as: :attachable, dependent: :destroy
-  belongs_to :user
 
   validates :title, :body, presence: true
-
-  accepts_nested_attributes_for :attachments, reject_if: :all_blank
 end
