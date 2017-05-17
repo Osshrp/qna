e87ce4336af643eb379a25937c65ae87920343a8
Osshrp <osshrp@gmail.com>
2017-05-17 16:18:20 +0300

    vote_for_question spec

--- spec/acceptance/vote_for_question_spec.rb
+++ spec/acceptance/vote_for_question_spec.rb
@@ -5,7 +5,21 @@ feature 'Vote for question', %q{
   As an authenticated user
   Id like to be able to vote for question
 } do
-  scenario 'authenticated user votes for question'
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  scenario 'authenticated user votes for question' do
+    sign_in(user)
+    visit question_path(question)
+    within '.vote' do
+      click_on 'Like'
+      expect(page).to have_content 'You like it'
+      expect(page).to have_content 'Recall vote'
+    end
+
+
+  end
   scenario 'authenticated user tries to vote for question once more time'
   scenario 'author tries to vote for his question'
   scenario 'unauthenticated user rires to vote for question'
