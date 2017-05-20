74857af4f41846a89fd8db59afdc879de9bcb4b8
Osshrp <osshrp@gmail.com>
2017-05-20 13:12:06 +0300

    more acceptance specs

--- spec/acceptance/vote_for_answer_spec.rb
+++ spec/acceptance/vote_for_answer_spec.rb
@@ -5,7 +5,57 @@ feature 'Vote for answer', %q{
   As an authenticated user
   Id like to be able to vote for answer
 } do
-  scenario 'authenticated user votes for answer'
-  scenario 'author tries to vote for his answer'
-  scenario 'unauthenticated user rires to vote for answer'
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given!(:answer) { create(:answer, question: question) }
+
+
+  scenario 'authenticated user set like for answer', js: true do
+    sign_in(user)
+    visit question_path(question)
+
+    within '.answers' do
+      choose 'Like'
+      click_on 'Vote'
+      expect(page).to have_content "You've liked it"
+      expect(page).to have_button 'Recall vote'
+    end
+  end
+
+  scenario 'authenticated user set dislike to answer', js: true do
+    sign_in(user)
+    visit question_path(question)
+    within '.answers' do
+      choose 'Dislike'
+      click_on 'Vote'
+      expect(page).to have_content "You've disliked it"
+      expect(page).to have_button 'Recall vote'
+    end
+  end
+
+  scenario 'authenticated user tries to vote for answer once more time', js: true do
+    sign_in(user)
+    visit question_path(question)
+    within '.answers' do
+      choose 'Like'
+      click_on 'Vote'
+      expect(page).to have_no_button 'Vote'
+    end
+  end
+
+  scenario 'author tries to vote for his answer' do
+    sign_in(answer.user)
+    visit question_path(question)
+    within '.answers' do
+      expect(page).to have_no_button 'Vote'
+    end
+  end
+
+  scenario 'unauthenticated user tries to vote for answer' do
+    visit question_path(question)
+    within '.answers' do
+      expect(page).to have_no_button 'Vote'
+    end
+  end
 end
