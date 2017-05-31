26cd479c383ecaa21d2fd0bdb39b11ec79775c60
Osshrp <osshrp@gmail.com>
2017-05-31 09:20:27 +0300

    add comments spec

--- spec/acceptance/create_comment_to_answer_spec.rb
+++ spec/acceptance/create_comment_to_answer_spec.rb
@@ -23,15 +23,82 @@ feature 'Create comment', %q{
         expect(page).to have_no_button 'Save comment'
       end
     end
-    scenario "tries to create comment to another user's answer"
+    scenario "tries to create comment to another user's answer", js: true do
+      sign_in(another_user)
+      visit question_path(question)
+
+      within "#answer-#{answer.id}" do
+        click_on 'New comment'
+        fill_in 'Comment body', with: 'Text text'
+        click_on 'Save comment'
+
+        expect(page).to have_content 'Text text'
+        expect(page).to have_no_button 'Save comment'
+      end
+    end
   end
 
   context 'multiple sessions' do
-    scenario "comment appears on another user's page"
-    scenario "comment appears on guest's page"
+    scenario "comment appears on another user's page", js: true do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('another_user') do
+        sign_in(another_user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('user') do
+        within "#answer-#{answer.id}" do
+          click_on 'New comment'
+          fill_in 'Comment body', with: 'Text text'
+          click_on 'Save comment'
+        end
+      end
+
+      Capybara.using_session('another_user') do
+        visit question_path(question)
+
+        within "#answer-#{answer.id}" do
+          expect(page).to have_content 'Text text'
+        end
+      end
+    end
+
+    scenario "comment appears on guest's page", js: true do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('guest') do
+        visit question_path(question)
+      end
+
+      Capybara.using_session('user') do
+        within "#answer-#{answer.id}" do
+          click_on 'New comment'
+          fill_in 'Comment body', with: 'Text text'
+          click_on 'Save comment'
+        end
+      end
+
+      Capybara.using_session('guest') do
+        visit question_path(question)
+
+        within "#answer-#{answer.id}" do
+          expect(page).to have_content 'Text text'
+        end
+      end
+    end
   end
 
   context 'Guest' do
-    scenario 'tries to create comment to answer'
+    scenario 'tries to create comment to answer' do
+      visit question_path(question)
+      expect(page).to have_no_link 'New comment'
+    end
   end
 end
