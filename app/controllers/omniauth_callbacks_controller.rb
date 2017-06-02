c8f9547aceec60bfb013ad8852de82179dacf337
Osshrp <osshrp@gmail.com>
2017-06-02 16:36:51 +0300

    to home

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,30 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    # @user = User.find_for_oauth(request.env['omniauth.auth'])
+    # if @user.persisted?
+    #   sign_in_and_redirect @user, event: :authentication
+    #   set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+    # end
+    authenticate_user('facebook')
+  end
+
+  def twitter
+    # render json: request.env['omniauth.auth']
+    authenticate_user('twitter')
+    # @user = User.find_for_oauth(request.env['omniauth.auth'])
+    # if @user.persisted?
+    #   sign_in_and_redirect @user, event: :authentication
+    #   set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+    # end
+  end
+
+  private
+
+  def authenticate_user(provider)
     @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
     end
   end
 end
