f2464b2e739fc0b0af87d2aa3745058e8d8d065d
Osshrp <osshrp@gmail.com>
2017-06-09 10:23:40 +0300

    to home

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -23,7 +23,7 @@ describe Ability do
     let(:user) { create(:user) }
     let(:other_user) { create(:user) }
     let!(:question) { create(:question, user: user) }
-    let(:answer) { create(:answer, question: question) }
+    let!(:answer) { create(:answer, question: question) }
 
     it { should_not be_able_to :manage, :all }
     it { should be_able_to :read, :all }
@@ -43,6 +43,9 @@ describe Ability do
     it { should_not be_able_to :destroy, create(:answer, user: other_user), user: user }
 
     it { should be_able_to :set_best, answer, user: user  }
-    # it { should_not be_able_to :set_best, create(:answer, question: question), user: other_user  }
+    it { should_not be_able_to :set_best, answer, user: other_user  }
+
+    it { should be_able_to :vote, question, user: other_user }
+    it { should_not be_able_to :vote, question, user: user }
   end
 end
