dd59e53b982fbaa2b8d6a1740fce26dc6dbb45e9
Osshrp <osshrp@gmail.com>
2017-06-12 14:09:13 +0300

    once more fix

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,9 +1,17 @@
 class AttachmentsController < ApplicationController
+  before_action :set_attachment, olny: :destroy
+
   respond_to :js
 
+  authorize_resource
+
   def destroy
-    @attachment = Attachment.find(params[:id])
-    authorize! :destroy, @attachment
     respond_with(@attachment.destroy)
+  end
+
+  private
+
+  def set_attachment
+    @attachment = Attachment.find(params[:id])
   end
 end
