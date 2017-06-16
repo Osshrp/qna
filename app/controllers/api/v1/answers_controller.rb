1c397ea302c6d94fd206f9832949cd46fd3ea2ec
Osshrp <osshrp@gmail.com>
2017-06-16 15:09:36 +0300

    to home

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::AnswersController < Api::V1::BaseController
-  before_action :load_question
+  before_action :load_question, only: :index
 
   skip_authorization_check
 
   def index
     @answers = @question.answers
     respond_with @answers, each_serializer: AnswersListSerializer
+  end
+
+  def show
+    @answer = Answer.find(params[:id])
+    respond_with @answer, serializer: SingleAnswerSerializer
   end
 
   private
