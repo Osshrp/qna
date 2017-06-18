39fb5f15ada89b6387f90bd44b0cee71b5f73009
Osshrp <osshrp@gmail.com>
2017-06-18 13:18:08 +0300

    question#create API

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,13 +1,30 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
+  before_action :set_questions, only: [:index, :create]
+
   skip_authorization_check
 
   def index
-    @questions = Question.all
+
     respond_with @questions, each_serializer: QuestionsListSerializer
   end
 
   def show
     @question = Question.find(params[:id])
     respond_with @question, serializer: SingleQuestionSerializer
+  end
+
+  def create
+    respond_with(@question = @questions.create(question_params),
+      serializer: SingleQuestionSerializer)
+  end
+
+  private
+
+  def set_questions
+    @questions = Question.all
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body, attachments_attributes: [:id, :file, :_destroy])
   end
 end
