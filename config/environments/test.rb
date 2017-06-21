8b9da119349fa6320a4ef03b6da9a67cbcba6169
Osshrp <osshrp@gmail.com>
2017-06-21 16:17:01 +0300

    to home

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,4 +41,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Delayed::Worker.delay_jobs = false
 end
