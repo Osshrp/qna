94111eea4dd248e779f6d5522e75e9be8d2dbfa5
Osshrp <osshrp@gmail.com>
2017-06-23 10:35:28 +0300

    subscriptions

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -14,11 +14,24 @@ RSpec.describe Question, type: :model do
 
   it_behaves_like 'votable'
 
+  let(:user) { create(:user) }
+  let!(:question) { create(:question) }
+
   describe '#subscribe' do
-    let(:user) { create(:user) }
+    it 'should subscribe author after create question' do
+      expect(question.subscriptions.first.user).to eq question.user
+    end
+
     it 'should subscribe user to question' do
-      subject.subscribe(subject.user)
-      expect(subject.subscriptions.first.user).to eq user
+      expect{ question.subscribe(user) }.to change(question.subscriptions, :count).by(1)
+    end
+  end
+
+  describe '#unsubscribe' do
+    before { create(:subscription, question: question, user: user) }
+
+    it 'should unsubscribe user to question' do
+      expect{ question.unsubscribe(user) }.to change(question.subscriptions, :count).by(-1)
     end
   end
 end
