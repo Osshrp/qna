781c5c2e5b884020fab15f267f94daa472b8bb4c
Osshrp <osshrp@gmail.com>
2017-06-25 15:17:01 +0300

    some minor changes

--- spec/mailers/answer_mailer_spec.rb
+++ spec/mailers/answer_mailer_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 RSpec.describe AnswerMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "digest" do
+    let(:user)  { create(:user) }
+    let(:answer) { create(:answer) }
+    let(:mail) { AnswerMailer.notify(user, answer) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("New answer to #{answer.question.title}")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["from@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Hi")
+    end
+  end
 end
