02895a1476fd0bae1805418c7c503b08b563b74e
Osshrp <osshrp@gmail.com>
2017-07-09 09:28:11 +0300

    change mail default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qna_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.default_url_options = { host: "qna.bobrptd.ru" }
+  config.action_mailer.default_url_options = { host: "80.82.58.3", port: "81" }
   config.action_mailer.default_options = {from: 'oss@bobrptd.ru'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
